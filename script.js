04e6e93d04456225efc772f550c3aa3742c1db06
Aliyul Manan <108641435+AliyulManan123@users.noreply.github.com>
2025-04-23 13:15:31 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const countdownDate = new Date("August 23, 2025 00:00:00 GMT+0700").getTime();
+
+const countdownFunction = setInterval(function() {
+    const now = new Date().getTime();
+    const distance = countdownDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.querySelector(".cont-days").textContent = days < 10 ? `0${days}` : days;
+    document.querySelector(".cont-hour").textContent = hours < 10 ? `0${hours}` : hours;
+    document.querySelector(".cont-minute").textContent = minutes < 10 ? `0${minutes}` : minutes;
+    document.querySelector(".cont-second").textContent = seconds < 10 ? `0${seconds}` : seconds;
+
+    if (distance < 0) {
+        clearInterval(countdownFunction);
+        document.getElementById("countdown").innerHTML = "HAPPY BIRTHDAY!";
+    }
+}, 1000);
